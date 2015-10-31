b64aab52ef02ba54140051b83d51f55e338d96e1
Alvin1992 <fanrongxin@Alvin.local>
2015-10-31 22:21:35 +0800

    ex38

---
+++ learn_python_the_hard_way/ex38.py
@@ -0,0 +1,25 @@
+ten_things = "Apple Oranges Crows Telephone Light Sugar"
+
+print "Wait there are not ten things in that list. Let's fix that."
+
+stuff = ten_things.split(' ')
+
+more_stuff = ["Day", "Night", "Song", "Fresbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+	next_one = more_stuff.pop()
+	print "Adding: ", next_one
+	stuff.append(next_one)
+	print "There are %d items now." % len(stuff)
+
+print "There we go: ", stuff
+print "Let's do something with stuff."
+
+print stuff[1]
+print stuff[-1]
+
+print stuff.pop()
+
+print ' '.join(stuff)
+
+print '#'.join(stuff[3:5])
